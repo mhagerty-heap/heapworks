56055bf458e2e70528189bbde9218dd59af30579
Mike Hagerty <mike.hagerty@heapanalytics.com>
2023-01-31 12:09:40 -0500

    Change by Heap to Demo by Heap

--- src/AppFooter.js
+++ src/AppFooter.js
@@ -5,8 +5,7 @@ export const AppFooter = (props) => {
     return (
         <div className="layout-footer">
             <img src={props.layoutColorMode === 'light' ? 'assets/layout/images/heapLogo.png' : 'assets/layout/images/heapLogo.png'} alt="Logo" height="20" className="mr-2" />
-            by
-            <span className="font-medium ml-2">Heap</span>
+            Demo by Heap
         </div>
     );
 }
