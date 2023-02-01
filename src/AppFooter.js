ac7ce2207c97a761b6438d8d9f8c0f2a6776978e
Mike Hagerty <mike.hagerty@heapanalytics.com>
2023-02-01 08:50:32 -0500

    Added link to Demo by Heap image

--- src/AppFooter.js
+++ src/AppFooter.js
@@ -4,8 +4,9 @@ export const AppFooter = (props) => {
 
     return (
         <div className="layout-footer">
-            <img src={props.layoutColorMode === 'light' ? 'assets/layout/images/heapLogo.png' : 'assets/layout/images/heapLogo.png'} alt="Logo" height="20" className="mr-2" />
-            Demo by Heap
+            <a href="https://docs.google.com/document/d/1ykR7qO8WesjHqvRk1Hi3nINibqTnT83qDkcs4Z9dV30/edit?usp=sharing">
+              <img src={props.layoutColorMode === 'light' ? 'assets/layout/images/heapLogo.png' : 'assets/layout/images/heapLogo.png'} alt="Logo" height="15" className="mr-2" />Demo By Heap
+            </a>
         </div>
     );
 }
